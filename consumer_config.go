09d62678d183804e23e76861d49e0ab090f6ab9a
Ivan Lyutov <ivanlyutov@gmail.com>
2014-10-28 13:21:02 +0200

    added consumer config, default values and consumer properties

--- consumer_config.go
+++ consumer_config.go
@@ -17,3 +17,65 @@
 
 package go_kafka_client
 
+type ConsumerConfig struct {
+	RefreshMetadataBackoffMs int32
+	SocketTimeout int32
+	SocketBufferSize int32
+	FetchSize int32
+	MaxFetchSize int32
+	NumConsumerFetchers int32
+	DefaultFetcherBackoffMs int32
+	AutoCommit bool
+	AutoCommitInterval int32
+	MaxQueuedChunks int32
+	MaxRebalanceRetries int32
+	AutoOffsetReset string
+	ConsumerTimeoutMs int32
+	MinFetchBytes int32
+	MaxFetchWaitMs int32
+	MirrorTopicsWhitelist string
+	MirrorTopicsBlacklist string
+	MirrorConsumerNumThreads int32
+	OffsetsChannelBackoffMs int32
+	OffsetsChannelSocketTimeoutMs int32
+	OffsetsCommitMaxRetries int32
+	OffsetsStorage string
+	MirrorTopicsWhitelistProp string
+	MirrorTopicsBlacklistProp string
+	ExcludeInternalTopics bool
+	DefaultPartitionAssignmentStrategy string /* select between "range", and "roundrobin" */
+	MirrorConsumerNumThreadsProp string
+	DefaultClientId string
+}
+
+func Default(config *ConsumerConfig) {
+	config.RefreshMetadataBackoffMs = 200
+	config.SocketTimeout = 30 * 1000
+	config.SocketBufferSize = 64 * 1024
+	config.FetchSize = 1024 * 1024
+	config.MaxFetchSize = 10 * config.FetchSize
+	config.NumConsumerFetchers = 1
+	config.DefaultFetcherBackoffMs = 1000
+	config.AutoCommit = true
+	config.AutoCommitInterval = 60 * 1000
+	config.MaxQueuedChunks = 2
+	config.MaxRebalanceRetries = 4
+	config.AutoOffsetReset = "largest"
+	config.ConsumerTimeoutMs = -1
+	config.MinFetchBytes = 1
+	config.MaxFetchWaitMs = 100
+	config.MirrorTopicsWhitelist = ""
+	config.MirrorTopicsBlacklist = ""
+	config.MirrorConsumerNumThreads = 1
+	config.OffsetsChannelBackoffMs = 1000
+	config.OffsetsChannelSocketTimeoutMs = 10000
+	config.OffsetsCommitMaxRetries = 5
+	config.OffsetsStorage = "zookeeper"
+
+	config.MirrorTopicsWhitelistProp = "mirror.topic.whitelist"
+	config.MirrorTopicsBlacklistProp = "mirror.topic.blacklist"
+	config.ExcludeInternalTopics = true
+	config.DefaultPartitionAssignmentStrategy = "range"/* select between "range", and "roundrobin" */
+	config.MirrorConsumerNumThreadsProp = "mirror.consumer.numthreads"
+	config.DefaultClientId = ""
+}
