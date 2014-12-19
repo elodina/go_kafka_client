1c1acf3fbf56d50e5febf5ce9e7d6f2f0a4abfde
Joe Stein <joe.stein@stealth.ly>
2014-12-19 01:59:43 -0500

    futzing with the sarama NewProducer

--- producers/producers.go
+++ producers/producers.go
@@ -5,7 +5,7 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -18,15 +18,17 @@
 package main
 
 import (
-	kafkaClient "github.com/stealthly/go_kafka_client"
-	"time"
-	"github.com/stealthly/go-kafka/producer"
+	"code.google.com/p/go-uuid/uuid"
 	"fmt"
-	"strconv"
+	"github.com/Shopify/sarama"
+	metrics "github.com/rcrowley/go-metrics"
+	kafkaClient "github.com/stealthly/go_kafka_client"
+	"net"
 	"os"
 	"os/signal"
-	metrics "github.com/rcrowley/go-metrics"
-	"net"
+	"runtime"
+	"strconv"
+	"time"
 )
 
 func resolveConfig() (string, string, string, int, time.Duration, string, time.Duration) {
@@ -61,6 +63,8 @@ func startMetrics(graphiteConnect string, graphiteFlushInterval time.Duration) {
 }
 
 func main() {
+	fmt.Println(("Starting Producer"))
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	numMessage := 0
 
 	zkConnect, brokerConnect, topic, numPartitions, sleepTime, graphiteConnect, graphiteFlushInterval := resolveConfig()
@@ -72,20 +76,58 @@ func main() {
 
 	kafkaClient.CreateMultiplePartitionsTopic(zkConnect, topic, numPartitions)
 
-	p := producer.NewKafkaProducer(topic, []string{brokerConnect})
-	defer p.Close()
-	go func() {
-		for {
-			if err := p.SendStringSync(fmt.Sprintf("message %d!", numMessage)); err != nil {
-				panic(err)
+	//p := producer.NewKafkaProducer(topic, []string{brokerConnect})
+
+	client, err := sarama.NewClient(uuid.New(), []string{brokerConnect}, sarama.NewClientConfig())
+	if err != nil {
+		panic(err)
+	}
+
+	config := sarama.NewProducerConfig()
+	config.FlushMsgCount = 8000
+	config.FlushFrequency = 30 * time.Millisecond
+	config.AckSuccesses = true
+	producer, err := sarama.NewProducer(client, config)
+	if err != nil {
+		panic(err)
+	}
+	//defer producer.Close()
+	//defer p.Close()
+	for i := 0; i < 1; i++ {
+		go func() {
+			for {
+				message := &sarama.MessageToSend{Topic: topic, Key: numMessage, Value: sarama.StringEncoder(fmt.Sprintf("message %d!", numMessage))}
+				//if err := p.SendStringSync(fmt.Sprintf("message %d!", numMessage)); err != nil {
+				//	panic(err)
+				//}
+				numMessage++
+				producer.Input() <- message
+				time.Sleep(sleepTime)
 			}
-			numMessage++
-			produceRate.Mark(1)
-			time.Sleep(sleepTime)
-		}
-	}()
+		}()
+	}
 
 	ctrlc := make(chan os.Signal, 1)
 	signal.Notify(ctrlc, os.Interrupt)
+	go func() {
+		start := time.Now()
+		count := 0
+		for {
+			select {
+			case error := <-producer.Errors():
+				fmt.Println(error)
+				produceRate.Mark(1)
+			case <-producer.Successes():
+				produceRate.Mark(1)
+				count++
+				elapsed := time.Since(start)
+				if elapsed.Seconds() >= 1 {
+					fmt.Println(fmt.Sprintf("Per Second %d", count))
+					count = 0
+					start = time.Now()
+				}
+			}
+		}
+	}()
 	<-ctrlc
 }
