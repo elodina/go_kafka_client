35a83888d0445662069ffac3c03799e51c5559c7
serejja <serejja@gmail.com>
2014-12-30 18:30:02 +0200

    initial syslog producer commit

---
+++ syslog/syslog.go
@@ -0,0 +1,108 @@
+/* Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	kafka "github.com/stealthly/go_kafka_client"
+	"os"
+	"os/signal"
+	"strings"
+	syslog "github.com/mcuadros/go-syslog"
+)
+
+var logLevel = flag.String("log.level", "info", "Log level for built-in logger.")
+var producerConfig = flag.String("producer.config", "", "Path to producer configuration file.")
+var numProducers = flag.Int("num.producers", 1, "Number of producers.")
+var queueSize = flag.Int("queue.size", 10000, "Number of messages that are buffered between the consumer and producer.")
+var topic = flag.String("topic", "", "Topic to produce messages into.")
+var format = flag.String("format", "rfc5424", "Message format. Either RFC5424 or RFC3164.")
+var tcpPort = flag.String("tcp.port", "5140", "TCP port to listen for incoming messages.")
+var tcpHost = flag.String("tcp.host", "0.0.0.0", "TCP host to listen for incoming messages.")
+
+func parseAndValidateArgs() *kafka.SyslogProducerConfig {
+	flag.Parse()
+
+	setLogLevel()
+	rfc5424 := "rfc5424"
+	rfc3164 := "rfc3164"
+
+	if *topic == "" {
+		fmt.Println("Topic is required.")
+		os.Exit(1)
+	}
+	if *producerConfig == "" {
+		fmt.Println("Producer config is required.")
+		os.Exit(1)
+	}
+	if *queueSize < 0 {
+		fmt.Println("Queue size should be equal or greater than 0")
+		os.Exit(1)
+	}
+
+	config := kafka.NewSyslogProducerConfig()
+	conf, err := kafka.ProducerConfigFromFile(*producerConfig)
+	if err != nil {
+		panic(err)
+	}
+	if err = conf.Validate(); err != nil {
+		panic(err)
+	}
+	config.ProducerConfig = conf
+	config.NumProducers = *numProducers
+	config.ChannelSize = *queueSize
+	config.Topic = *topic
+	if strings.ToLower(*format) == rfc5424 {
+		config.Format = syslog.RFC5424
+	} else if strings.ToLower(*format) == rfc3164 {
+		config.Format = syslog.RFC3164
+	} else {
+		fmt.Println("Message format can be RFC5424 or RFC3164 (any case).")
+		os.Exit(1)
+	}
+	config.TCPAddr = fmt.Sprintf("%s:%s", *tcpHost, *tcpPort)
+
+	return config
+}
+
+func setLogLevel() {
+	var level kafka.LogLevel
+	switch strings.ToLower(*logLevel) {
+		case "trace": level = kafka.TraceLevel
+		case "debug": level = kafka.DebugLevel
+		case "info": level = kafka.DebugLevel
+		case "warn": level = kafka.DebugLevel
+		case "error": level = kafka.DebugLevel
+		case "critical": level = kafka.DebugLevel
+		default: {
+			fmt.Printf("Invalid log level: %s\n", *logLevel)
+			os.Exit(1)
+		}
+	}
+	kafka.Logger = kafka.NewDefaultLogger(level)
+}
+
+func main() {
+	config := parseAndValidateArgs()
+	producer := kafka.NewSyslogProducer(config)
+	go producer.Start()
+
+	ctrlc := make(chan os.Signal, 1)
+	signal.Notify(ctrlc, os.Interrupt)
+	<-ctrlc
+	producer.Stop()
+}
---
+++ syslog_producer.go
@@ -0,0 +1,153 @@
+/* Licensed to the Apache Software Foundation (ASF) under one or more
+ contributor license agreements.  See the NOTICE file distributed with
+ this work for additional information regarding copyright ownership.
+ The ASF licenses this file to You under the Apache License, Version 2.0
+ (the "License"); you may not use this file except in compliance with
+ the License.  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License. */
+
+package go_kafka_client
+
+import (
+	syslog "github.com/mcuadros/go-syslog"
+	"github.com/Shopify/sarama"
+	"strings"
+	"encoding/json"
+)
+
+// SyslogProducerConfig defines configuration options for SyslogProducer
+type SyslogProducerConfig struct {
+	// Syslog producer config.
+	ProducerConfig *ProducerConfig
+
+	// Number of producer instances.
+	NumProducers int
+
+	// Number of messages that are buffered to produce.
+	ChannelSize int
+
+	// Message format. Either RFC5424 or RFC3164
+	Format syslog.Format
+
+	Topic string
+
+	// Receive messages from this address and post them to topic.
+	TCPAddr string
+}
+
+// Creates an empty SyslogProducerConfig.
+func NewSyslogProducerConfig() *SyslogProducerConfig {
+	return &SyslogProducerConfig{}
+}
+
+type SyslogProducer struct {
+	config *SyslogProducerConfig
+	server *syslog.Server
+	incoming syslog.LogPartsChannel
+
+	producers []*sarama.Producer
+}
+
+func NewSyslogProducer(config *SyslogProducerConfig) *SyslogProducer {
+	return &SyslogProducer{
+		config: config,
+	}
+}
+
+func (this *SyslogProducer) String() string {
+	return "syslog-producer"
+}
+
+func (this *SyslogProducer) Start() {
+	Trace(this, "Starting...")
+	this.startTCPServer()
+	this.startProducers()
+}
+
+func (this *SyslogProducer) Stop() {
+	Trace(this, "Stopping..")
+	close(this.incoming)
+
+	for _, producer := range this.producers {
+		producer.Close()
+	}
+
+	this.server.Kill()
+}
+
+func (this *SyslogProducer) startTCPServer() {
+	Trace(this, "Starting TCP server")
+	channel := make(syslog.LogPartsChannel)
+	handler := syslog.NewChannelHandler(channel)
+
+	server := syslog.NewServer()
+	server.SetFormat(this.config.Format)
+	server.SetHandler(handler)
+	if err := server.ListenTCP(this.config.TCPAddr); err != nil {
+		panic(err)
+	}
+	if err := server.Boot(); err != nil {
+		panic(err)
+	}
+
+	this.server = server
+	this.incoming = channel
+	Infof(this, "Listening for messages at TCP %s", this.config.TCPAddr)
+}
+
+func (this *SyslogProducer) startProducers() {
+	for i := 0; i < this.config.NumProducers; i++ {
+		conf := this.config.ProducerConfig
+		client, err := sarama.NewClient(conf.Clientid, conf.BrokerList, sarama.NewClientConfig())
+		if err != nil {
+			panic(err)
+		}
+
+		config := sarama.NewProducerConfig()
+		config.ChannelBufferSize = conf.SendBufferSize
+		switch strings.ToLower(conf.CompressionCodec) {
+		case "none":
+			config.Compression = sarama.CompressionNone
+		case "gzip":
+			config.Compression = sarama.CompressionGZIP
+		case "snappy":
+			config.Compression = sarama.CompressionSnappy
+		}
+		config.FlushByteCount = conf.FlushByteCount
+		config.FlushFrequency = conf.FlushTimeout
+		config.FlushMsgCount = conf.BatchSize
+		config.MaxMessageBytes = conf.MaxMessageBytes
+		config.MaxMessagesPerReq = conf.MaxMessagesPerRequest
+		config.Partitioner = sarama.NewRandomPartitioner
+		config.RequiredAcks = sarama.RequiredAcks(conf.Acks)
+		config.RetryBackoff = conf.RetryBackoff
+		config.Timeout = conf.Timeout
+
+		producer, err := sarama.NewProducer(client, config)
+		if err != nil {
+			panic(err)
+		}
+		this.producers = append(this.producers, producer)
+		go this.produceRoutine(producer)
+	}
+}
+
+func (this *SyslogProducer) produceRoutine(producer *sarama.Producer) {
+	for msg := range this.incoming {
+		Tracef(this, "Got message: %s", msg)
+		//TODO custom transformations
+		b, err := json.Marshal(msg)
+		if err != nil {
+			Errorf(this, "Failed to marshal %s as JSON", msg)
+		}
+
+		producer.Input() <- &sarama.MessageToSend{Topic: this.config.Topic, Value: sarama.ByteEncoder(b)}
+	}
+}
