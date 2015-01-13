bfb907d8069b5aa9876b0d0a7f51702a3f174ff7
serejja <serejja@gmail.com>
2015-01-13 16:52:41 +0200

    working on vagrant and docker for syslog

--- vagrant/kafka.sh
+++ vagrant/kafka.sh
@@ -15,20 +15,16 @@
 
 #!/bin/sh -Eux
 
-# Start Zookeeper and Kafka
-./vagrant/kafka.sh
+#Start Zookeeper
+echo 'Starting Zookeeper'
+$ZK_HOME/bin/zkServer.sh start
 
-mkdir -p $GOPATH/src/github.com/stealthly/go_kafka_client
-cp -r /go_kafka_client $GOPATH/src/github.com/stealthly
-cd $GOPATH/src/github.com/stealthly/go_kafka_client
+#Start Kafka
+sed -r -i "s/(zookeeper.connect)=(.*)/\1=$ZK_PORT_2181_TCP_ADDR/g" $KAFKA_PATH/config/server.properties
+sed -r -i "s/(broker.id)=(.*)/\1=$BROKER_ID/g" $KAFKA_PATH/config/server.properties
+sed -r -i "s/#(advertised.host.name)=(.*)/\1=$HOST_IP/g" $KAFKA_PATH/config/server.properties
+sed -r -i "s/^(port)=(.*)/\1=$PORT/g" $KAFKA_PATH/config/server.properties
+sed -r -i "s/^(log4j.rootLogger)=(.*)( stdout)/\1=WARN\3/g" $KAFKA_PATH/config/log4j.properties
 
-echo 'Updating dependencies'
-gpm install
-
-echo 'Running tests'
-go test -v
-
-echo 'Stopping Kafka'
-$KAFKA_PATH/bin/kafka-server-stop.sh
-echo 'Stopping Zookeeper'
-$ZK_HOME/bin/zkServer.sh stop
+echo 'Starting Kafka'
+$KAFKA_PATH/bin/kafka-server-start.sh $KAFKA_PATH/config/server.properties &
