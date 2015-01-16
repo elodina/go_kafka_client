dc9d7d12bd2fdbeca1d9f6a234597c2a2e842d2c
serejja <serejja@gmail.com>
2015-01-16 15:50:11 +0200

    vagrant fix

--- syslog/vagrant/syslog.sh
+++ syslog/vagrant/syslog.sh
@@ -30,37 +30,23 @@ exitscript()
         exit 0
 }
 
-# Syslog Server configurations here.
-PRODUCER_CONFIG="/vagrant/syslog/producer.properties"
-TOPIC="syslog"
-FORMAT="rfc5424"
-TCP_PORT="5140"
-TCP_HOST="0.0.0.0"
-UDP_PORT="5141"
-UDP_HOST="0.0.0.0"
-NUM_PRODUCERS="1"
-QUEUE_SIZE="10000"
-LOG_LEVEL="info"
-MAX_PROCS="0" # 0 does not modify current value, it's here just to expose the parameter
+# Apply Syslog Server configurations.
+source /vagrant/syslog/vagrant/params.sh
 
 # Setup Zookeeper, Kafka, Go etc.
-export HOST_IP=192.168.66.66
-export KAFKA_VERSION=0.8.2-beta
-export SCALA_VERSION=2.10
-export KAFKA_RELEASE=kafka_$SCALA_VERSION-$KAFKA_VERSION
-export KAFKA_PATH=/opt/$KAFKA_RELEASE
-export ZK_VERSION=3.4.6
-export ZK_RELEASE=zookeeper-$ZK_VERSION
-export ZK_HOME=/opt/$ZK_RELEASE
+export HOST_IP_ADDR=192.168.66.66
+source /vagrant/vagrant/prep.sh
 
-/vagrant/vagrant/prep.sh
+#Build syslog binary
+cd $GOPATH/src/github.com/stealthly/go_kafka_client && gpm install
+cd $GOPATH/src/github.com/stealthly/go_kafka_client/syslog && go build
 
 #Start Zookeeper
 echo 'Starting Zookeeper'
 $ZK_HOME/bin/zkServer.sh start
 
 #Start Kafka
-echo "advertised.host.name=$HOST_IP" >> $KAFKA_PATH/config/server.properties
+echo "advertised.host.name=$HOST_IP_ADDR" >> $KAFKA_PATH/config/server.properties
 sed -r -i "s/^(log4j.rootLogger)=(.*)( stdout)/\1=WARN\3/g" $KAFKA_PATH/config/log4j.properties
 
 echo 'Starting Kafka'
@@ -69,6 +55,6 @@ $KAFKA_PATH/bin/kafka-server-start.sh $KAFKA_PATH/config/server.properties &
 # let Zookeeper and Kafka start normally
 sleep 5
 $KAFKA_PATH/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic $TOPIC
-/vagrant/syslog/syslog --producer.config $PRODUCER_CONFIG --topic $TOPIC --format $FORMAT --tcp.port $TCP_PORT --tcp.host $TCP_HOST --udp.port $UDP_PORT --udp.host $UDP_HOST --num.producers $NUM_PRODUCERS --queue.size $QUEUE_SIZE --log.level $LOG_LEVEL --max.procs $MAX_PROCS &
+$GOPATH/src/github.com/stealthly/go_kafka_client/syslog/syslog $SYSLOG_ARGS &
 
 exitscript
