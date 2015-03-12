7f45fc9fb675bb0f44606b48fbdbcc7124450645
Ivan Lyutov <ivanlyutov@gmail.com>
2015-03-12 14:14:42 +0200

    Added basic entities

--- schema_registry.go
+++ schema_registry.go
@@ -2,7 +2,7 @@
 contributor license agreements.  See the NOTICE file distributed with
 this work for additional information regarding copyright ownership.
 The ASF licenses this file to You under the Apache License, Version 2.0
-(the "License"); you may not use this file except in compliance with
+(the "License") you may not use this file except in compliance with
 the License.  You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
@@ -17,17 +17,82 @@ package go_kafka_client
 
 import (
 	avro "github.com/stealthly/go-avro"
+	"net/http"
 )
 
 type SchemaRegistryClient interface {
 	Register(subject string, schema avro.Schema) int32
 	GetByID(id int32) avro.Schema
 	GetLatestSchemaMetadata(subject string) *SchemaMetadata
-	GetVersion(subject string, schema avro.Schema)
+	GetVersion(subject string, schema avro.Schema) int32
 }
 
 type SchemaMetadata struct {
-	Id int32
+	Id      int32
 	Version int32
-	Schema string
+	Schema  string
+}
+
+type CompatibilityLevel string
+
+const (
+	BackwardCompatibilityLevel CompatibilityLevel = "BACKWARD"
+	ForwardCompatibilityLevel CompatibilityLevel  = "FORWARD"
+	FullCompatibilityLevel CompatibilityLevel     = "FULL"
+	NoneCompatibilityLevel CompatibilityLevel     = "NONE"
+)
+
+type Version string
+
+const (
+	SCHEMA_REGISTRY_V1_JSON Version               = "application/vnd.schemaregistry.v1+json"
+	SCHEMA_REGISTRY_V1_JSON_WEIGHTED Version      = "application/vnd.schemaregistry.v1+json"
+	SCHEMA_REGISTRY_MOST_SPECIFIC_DEFAULT Version = "application/vnd.schemaregistry.v1+json"
+	SCHEMA_REGISTRY_DEFAULT_JSON Version          = "application/vnd.schemaregistry+json"
+	SCHEMA_REGISTRY_DEFAULT_JSON_WEIGHTED Version = "application/vnd.schemaregistry+json qs=0.9"
+	JSON Version                                  = "application/json"
+	JSON_WEIGHTED Version                         = "application/json qs=0.5"
+	GENERIC_REQUEST Version                       = "application/octet-stream"
+)
+
+var PREFERRED_RESPONSE_TYPES = [...]Version {SCHEMA_REGISTRY_V1_JSON, SCHEMA_REGISTRY_DEFAULT_JSON, JSON}
+
+type ErrorMessage struct {
+	ErrorCode int32
+	Message   string
+}
+
+type Schema struct {
+	Subject string
+	Version int32
+	Id      int32
+	Schema  string
+}
+
+type CachedSchemaRegistryClient struct {
+	RegistryURL string
+	httpClient  *http.Client
+}
+
+func NewCachedSchemaRegistryClient(registryURL string) *CachedSchemaRegistryClient {
+	return &CachedSchemaRegistryClient{
+		RegistryURL: registryURL,
+		httpClient: http.DefaultClient,
+	}
+}
+
+func (this *CachedSchemaRegistryClient) Register(subject string, schema avro.Schema) int32 {
+	return 0
+}
+
+func (this *CachedSchemaRegistryClient) GetByID(id int32) avro.Schema {
+	return nil
+}
+
+func (this *CachedSchemaRegistryClient) GetLatestSchemaMetadata(subject string) *SchemaMetadata {
+	return nil
+}
+
+func (this *CachedSchemaRegistryClient) GetVersion(subject string, schema avro.Schema) int32 {
+	return 0
 }
