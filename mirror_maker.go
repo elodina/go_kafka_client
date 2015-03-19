9185ee32c0f5b1526b9b3deff3adf19e1d680d76
Ivan Lyutov <ivanlyutov@gmail.com>
2015-03-19 19:20:18 +0200

    re #23 rollong back to interface

--- mirror_maker.go
+++ mirror_maker.go
@@ -298,16 +298,16 @@ func (this *MirrorMaker) addTiming(record *avro.GenericRecord) *avro.GenericReco
 		this.newSchema = &schema
 		this.newSchema.Fields = append(this.newSchema.Fields, TimingField)
 	}
-	var timings []int64
+	var timings []interface {}
 	if record.Get("timings") == nil {
-		timings = make([]int64, 0)
+		timings = make([]interface {}, 0)
 		newRecord := avro.NewGenericRecord(this.newSchema)
 		for _, field := range this.newSchema.Fields {
 			newRecord.Set(field.Name, record.Get(field.Name))
 		}
 		record = newRecord
 	} else {
-		timings = record.Get("timings").([]int64)
+		timings = record.Get("timings").([]interface {})
 	}
 	timings = append(timings, now)
 	record.Set("timings", timings)
