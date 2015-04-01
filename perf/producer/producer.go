6ef3d6f692399b5ab4578ac6f8fd5fdf39972454
serejja <serejja@gmail.com>
2015-04-01 20:16:07 +0300

    go fmt

--- perf/producer/producer.go
+++ perf/producer/producer.go
@@ -16,15 +16,15 @@ limitations under the License. */
 package main
 
 import (
-    "flag"
-    kafka "github.com/stealthly/go_kafka_client"
-    sp "github.com/stealthly/go_kafka_client/syslog/syslog_proto"
-    "fmt"
-    "os"
-    "github.com/stealthly/go-avro"
-    "time"
-    "strings"
-    "github.com/golang/protobuf/proto"
+	"flag"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"github.com/stealthly/go-avro"
+	kafka "github.com/stealthly/go_kafka_client"
+	sp "github.com/stealthly/go_kafka_client/syslog/syslog_proto"
+	"os"
+	"strings"
+	"time"
 )
 
 var brokerList = flag.String("broker.list", "", "Broker List to produce messages too.")
@@ -36,138 +36,137 @@ var protobuf = flag.Bool("proto", true, "Flag to send messages as protocol buffe
 var perSecond = flag.Int("msg.per.sec", 0, "Messages per second to send.")
 
 func main() {
-    parseAndValidateArgs()
+	parseAndValidateArgs()
 
-    if *protobuf {
-        produceProtobuf()
-    } else {
-        produceAvro()
-    }
+	if *protobuf {
+		produceProtobuf()
+	} else {
+		produceAvro()
+	}
 }
 
 func produceProtobuf() {
-    config1 := kafka.DefaultProducerConfig()
-    config1.BrokerList = strings.Split(*brokerList, ",")
-    config1.AckSuccesses = true
-    producer1 := kafka.NewSaramaProducer(config1)
+	config1 := kafka.DefaultProducerConfig()
+	config1.BrokerList = strings.Split(*brokerList, ",")
+	config1.AckSuccesses = true
+	producer1 := kafka.NewSaramaProducer(config1)
 
-    config2 := kafka.DefaultProducerConfig()
-    config2.BrokerList = strings.Split(*brokerList, ",")
-    producer2 := kafka.NewSaramaProducer(config2)
+	config2 := kafka.DefaultProducerConfig()
+	config2.BrokerList = strings.Split(*brokerList, ",")
+	producer2 := kafka.NewSaramaProducer(config2)
 
-    go func() {
-        for message := range producer1.Successes() {
-            line := &sp.LogLine{}
-            proto.Unmarshal(message.Value.([]byte), line)
-            line.Timings = append(line.Timings, time.Now().UnixNano()/int64(time.Millisecond))
-            bytes, err := proto.Marshal(line)
-            if err != nil {
-                panic(err)
-            }
+	go func() {
+		for message := range producer1.Successes() {
+			line := &sp.LogLine{}
+			proto.Unmarshal(message.Value.([]byte), line)
+			line.Timings = append(line.Timings, time.Now().UnixNano()/int64(time.Millisecond))
+			bytes, err := proto.Marshal(line)
+			if err != nil {
+				panic(err)
+			}
 
-            producer2.Input() <- &kafka.ProducerMessage{ Topic: *topic2, Value: bytes }
-        }
-    }()
+			producer2.Input() <- &kafka.ProducerMessage{Topic: *topic2, Value: bytes}
+		}
+	}()
 
+	for _ = range time.Tick(1 * time.Second) {
+		messagesSent := 0
+		for messagesSent < *perSecond {
+			line := &sp.LogLine{}
+			line.Line = proto.String("")
+			line.Timings = []int64{time.Now().UnixNano() / int64(time.Millisecond)}
+			bytes, err := proto.Marshal(line)
+			if err != nil {
+				panic(err)
+			}
 
-    for _ = range time.Tick(1 * time.Second) {
-        messagesSent := 0
-        for messagesSent < *perSecond {
-            line := &sp.LogLine{}
-            line.Line = proto.String("")
-            line.Timings = []int64{time.Now().UnixNano()/int64(time.Millisecond)}
-            bytes, err := proto.Marshal(line)
-            if err != nil {
-                panic(err)
-            }
-
-            message := &kafka.ProducerMessage{ Topic: *topic1, Value: bytes}
-            producer1.Input() <- message
-            messagesSent++
-        }
-    }
+			message := &kafka.ProducerMessage{Topic: *topic1, Value: bytes}
+			producer1.Input() <- message
+			messagesSent++
+		}
+	}
 }
 
 func produceAvro() {
-    config1 := kafka.DefaultProducerConfig()
-    config1.BrokerList = strings.Split(*brokerList, ",")
-    config1.ValueEncoder = kafka.NewKafkaAvroEncoder(*schemaRegistry)
-    config1.AckSuccesses = true
+	config1 := kafka.DefaultProducerConfig()
+	config1.BrokerList = strings.Split(*brokerList, ",")
+	config1.ValueEncoder = kafka.NewKafkaAvroEncoder(*schemaRegistry)
+	config1.AckSuccesses = true
 
-    producer1 := kafka.NewSaramaProducer(config1)
+	producer1 := kafka.NewSaramaProducer(config1)
 
-    config2 := kafka.DefaultProducerConfig()
-    config2.BrokerList = strings.Split(*brokerList, ",")
-    config2.ValueEncoder = kafka.NewKafkaAvroEncoder(*schemaRegistry)
-    producer2 := kafka.NewSaramaProducer(config2)
+	config2 := kafka.DefaultProducerConfig()
+	config2.BrokerList = strings.Split(*brokerList, ",")
+	config2.ValueEncoder = kafka.NewKafkaAvroEncoder(*schemaRegistry)
+	producer2 := kafka.NewSaramaProducer(config2)
 
-    avroSchema, err := avro.ParseSchemaFile(*avroSchema)
-    if err != nil {
-        panic(err)
-    }
+	avroSchema, err := avro.ParseSchemaFile(*avroSchema)
+	if err != nil {
+		panic(err)
+	}
 
-    decoder := kafka.NewKafkaAvroDecoder(*schemaRegistry)
-    go func() {
-        for message := range producer1.Successes() {
-            rawRecord, err := decoder.Decode(message.Value.([]byte))
-            if err != nil {
-                panic(err)
-            }
-            record := rawRecord.(*avro.GenericRecord)
-            timings := record.Get("timings").([]interface{})
-            timings = append(timings, time.Now().UnixNano())
-            record.Set("timings", timings)
+	decoder := kafka.NewKafkaAvroDecoder(*schemaRegistry)
+	go func() {
+		for message := range producer1.Successes() {
+			rawRecord, err := decoder.Decode(message.Value.([]byte))
+			if err != nil {
+				panic(err)
+			}
+			record := rawRecord.(*avro.GenericRecord)
+			timings := record.Get("timings").([]interface{})
+			timings = append(timings, time.Now().UnixNano())
+			record.Set("timings", timings)
 
-            producer2.Input() <- &kafka.ProducerMessage{ Topic: *topic2, Value: record }
-        }
-    }()
+			producer2.Input() <- &kafka.ProducerMessage{Topic: *topic2, Value: record}
+		}
+	}()
 
-    for _ = range time.Tick(1 * time.Second) {
-        messagesSent := 0
-        for messagesSent < *perSecond {
-            record := avro.NewGenericRecord(avroSchema)
-            record.Set("id", int64(0))
-            record.Set("timings", []int64{time.Now().UnixNano()/int64(time.Millisecond)})
-            record.Set("value", []byte{})
+	for _ = range time.Tick(1 * time.Second) {
+		messagesSent := 0
+		for messagesSent < *perSecond {
+			record := avro.NewGenericRecord(avroSchema)
+			record.Set("id", int64(0))
+			record.Set("timings", []int64{time.Now().UnixNano() / int64(time.Millisecond)})
+			record.Set("value", []byte{})
 
-            message := &kafka.ProducerMessage{ Topic: *topic1, Value: record}
-            producer1.Input() <- message
-            messagesSent++
-        }
-    }
+			message := &kafka.ProducerMessage{Topic: *topic1, Value: record}
+			producer1.Input() <- message
+			messagesSent++
+		}
+	}
 }
 
 func parseAndValidateArgs() {
-    flag.Parse()
-    if *brokerList == "" {
-        fmt.Println("Broker list is required")
-        os.Exit(1)
-    }
+	flag.Parse()
+	if *brokerList == "" {
+		fmt.Println("Broker list is required")
+		os.Exit(1)
+	}
 
-    if !*protobuf {
-        if *schemaRegistry == "" {
-            fmt.Println("Schema registry is required")
-            os.Exit(1)
-        }
+	if !*protobuf {
+		if *schemaRegistry == "" {
+			fmt.Println("Schema registry is required")
+			os.Exit(1)
+		}
 
-        if *avroSchema == "" {
-            fmt.Println("Avro schema is required")
-            os.Exit(1)
-        }
-    }
+		if *avroSchema == "" {
+			fmt.Println("Avro schema is required")
+			os.Exit(1)
+		}
+	}
 
-    if *perSecond <= 0 {
-        fmt.Println("Messages per second should be greater than 0")
-        os.Exit(1)
-    }
+	if *perSecond <= 0 {
+		fmt.Println("Messages per second should be greater than 0")
+		os.Exit(1)
+	}
 
-    if *topic1 == "" {
-        fmt.Println("Topic 1 is required")
-        os.Exit(1)
-    }
+	if *topic1 == "" {
+		fmt.Println("Topic 1 is required")
+		os.Exit(1)
+	}
 
-    if *topic2 == "" {
-        fmt.Println("Topic 2 is required")
-        os.Exit(1)
-    }
+	if *topic2 == "" {
+		fmt.Println("Topic 2 is required")
+		os.Exit(1)
+	}
 }
