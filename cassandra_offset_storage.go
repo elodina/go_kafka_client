afdc3a342d72e473ba3b325a99149f64cec3e0f9
ilyutov <ivanlyutov@gmail.com>
2015-05-29 16:49:45 +0300

    Additional field updated.

--- cassandra_offset_storage.go
+++ cassandra_offset_storage.go
@@ -26,7 +26,7 @@ func (this *CassandraOffsetStorage) GetOffset (group string, topic string, parti
     this.ensureConnection(group)
     response := &OffsetResponse{}
     data := this.connection.Query(fmt.Sprintf("SELECT * FROM %s WHERE topic = ? AND partition = ? LIMIT 1", strings.Replace(group, "-", "", -1)), topic, partition).Iter()
-    data.Scan(&response.Topic, &response.Partition, &response.LastOffset, &response.TraceData)
+    data.Scan(&response.Topic, &response.Partition, &response.LastOffset, &response.TraceData, &response.Updated)
     if err := data.Close(); err != nil {
         return InvalidOffset, err
     }
@@ -38,7 +38,7 @@ func (this *CassandraOffsetStorage) GetOffset (group string, topic string, parti
 // May return an error if fails to commit the offset.
 func (this *CassandraOffsetStorage) CommitOffset (group string, topic string, partition int32, offset int64) error {
     this.ensureConnection(group)
-    return this.connection.Query(fmt.Sprintf("UPDATE %s SET last_offset = ? WHERE topic = ? AND partition = ?", strings.Replace(group, "-", "", -1)), offset, topic, partition).Exec()
+    return this.connection.Query(fmt.Sprintf("UPDATE %s SET last_offset = ?, updated = dateof(now()) WHERE topic = ? AND partition = ?", strings.Replace(group, "-", "", -1)), offset, topic, partition).Exec()
 }
 
 func (this *CassandraOffsetStorage) ensureConnection(consumerGroup string) {
@@ -50,7 +50,7 @@ func (this *CassandraOffsetStorage) ensureConnection(consumerGroup string) {
                 if err != nil {
                     panic(err)
                 }
-                query := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s(topic text, partition int, last_offset int, trace_data blob, PRIMARY KEY(topic, partition))", strings.Replace(consumerGroup, "-", "", -1))
+                query := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s(topic text, partition int, last_offset int, trace_data blob, updated timestamp, PRIMARY KEY(topic, partition))", strings.Replace(consumerGroup, "-", "", -1))
                 this.connection.Query(query).Exec()
             }
         })
@@ -62,4 +62,5 @@ type OffsetResponse struct {
     Partition int32
     LastOffset int64
     TraceData []byte
+    Updated int64
 }
