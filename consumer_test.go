caa53ca16854febbecaa61b93428d01a7be5c525
ilyutov <ivanlyutov@gmail.com>
2015-07-10 15:39:32 +0300

    updated timeouts, reduced barrier timeout.

--- consumer_test.go
+++ consumer_test.go
@@ -328,11 +328,13 @@ func TestBlueGreenDeployment(t *testing.T) {
 	greenGroupConsumers := []*Consumer{createConsumerForGroup(greenGroup, activeStrategy), createConsumerForGroup(greenGroup, activeStrategy)}
 
 	for _, consumer := range blueGroupConsumers {
+		consumer.config.BarrierTimeout = 10 * time.Second
 		go consumer.StartStatic(map[string]int{
 			activeTopic: 1,
 		})
 	}
 	for _, consumer := range greenGroupConsumers {
+		consumer.config.BarrierTimeout = 10 * time.Second
 		go consumer.StartStatic(map[string]int{
 			inactiveTopic: 1,
 		})
@@ -345,7 +347,7 @@ func TestBlueGreenDeployment(t *testing.T) {
 
 	coordinator.RequestBlueGreenDeployment(blue, green)
 
-	time.Sleep(60 * time.Second)
+	time.Sleep(30 * time.Second)
 
 	//All Blue consumers should switch to Green group and change topic to inactive
 	greenConsumerIds, _ := coordinator.GetConsumersInGroup(greenGroup)
@@ -373,8 +375,6 @@ func TestBlueGreenDeployment(t *testing.T) {
 
 	//At this stage Blue group became Green group
 	//and Green group became Blue group
-
-	time.Sleep(10 * time.Second)
 
 	//Producing messages to both topics
 	produceMessages := 10
